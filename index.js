874e8a5c9ebbb058aa30194b8cdcdbc85d34de39
Jose Padron <josepadron.go@gmail.com>
2019-04-08 22:47:41 -0500

    adding emailJS

--- index.js
+++ index.js
@@ -1,3 +1,25 @@
 $(function () {
   $('[data-toggle="tooltip"]').tooltip()
 })
+
+const $form = document.getElementById('contact-form');
+
+$("#contact-form").on("submit", function(event) {
+  event.preventDefault(); // prevent reload
+  
+  var formData = new FormData(this);
+  formData.append('service_id', 'josepadron_go');
+  formData.append('template_id', 'respuesta_de_cv');
+  formData.append('user_id', 'user_kjZM1hD2hofBgOU4dsXlV');
+
+  $.ajax('https://api.emailjs.com/api/v1.0/email/send-form', {
+      type: 'POST',
+      data: formData,
+      contentType: false, // auto-detection
+      processData: false // no need to parse formData to string
+  }).done(function() {
+      alert('Your mail is sent!');
+  }).fail(function(error) {
+      alert('Oops... ' + JSON.stringify(error));
+  });
+});
